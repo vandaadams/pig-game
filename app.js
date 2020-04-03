3031271cf2ddb766c49b5f96ddbfd07c7644429b
Vanda Adams <vandamadams@gmail.com>
2020-04-03 18:41:26 +0100

    added js for current score counter

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const scores = [0, 0];
-const roundScore = 0;
-const activePlayer = 0;
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+let scores = [0, 0];
+let roundScore = 0;
+let activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -13,10 +11,19 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', btn = () => {
   // creates random number between 1 and 6
-  const dice = Math.floor(Math.random() * 6) + 1;
+  let dice = Math.floor(Math.random() * 6) + 1;
   // displays result
   const diceDOM = document.querySelector('.dice');
   diceDOM.style.display = 'block';
   diceDOM.src = 'assets/dice-' + dice + '.png';
   //updates round score if the rolled number was not 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+  }
 });
